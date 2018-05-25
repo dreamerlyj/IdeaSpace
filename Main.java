a9a8fe7cefa385183020bfe4eb1cf78d89109382
dreamerlyj <17610602373@163.com>
2018-05-25 13:31:16 +0800

    test

--- Main.java
+++ Main.java
@@ -1,14 +1,35 @@
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("test");
-        System.out.println();
         Main main = new Main();
         main.testIt();
+        testMap();
     }
     private void testIt(){
         System.out.println("分支提交");
         System.out.println("主干提交");
         System.out.println("test");
+    }
+
+    private static void testMap() {
+        Map<String,Map<String,String>> map = new HashMap<>();
+        List list = new ArrayList();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+
+        for (int i = 0; i < list.size(); i++) {
+            Map<String, String> selectMap = map.get(list.get(i));
+            if (selectMap == null) {
+                selectMap = new HashMap<>();
+                System.out.println(selectMap);
+                map.put(list.get(i).toString(),selectMap);
+            }
+            selectMap.put("extra", list.get(i).toString());
+        }
+        System.out.println(map);
     }
 }
